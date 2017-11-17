4ebe995b5335ff796412cfd2c21ded86a0351f5c
Hironori Tanimura <over88keys@yahoo.co.jp>
2017-11-17 07:22:45 +0000

    userspage fixed

--- resources/views/favorite/favorite_button.blade.php
+++ resources/views/favorite/favorite_button.blade.php
@@ -1,7 +1,7 @@
 <!--@if (Auth::user()->id != $user->id) -->
     @if (Auth::user()->is_favorite($micropost->id))
         {!! Form::open(['route' => ['user.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-xs"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-warning btn-xs"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.favorite', $micropost->id]]) !!}

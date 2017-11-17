c348efd7eb5425d3d786dfaaef50ae4736460963
Hironori Tanimura <over88keys@yahoo.co.jp>
2017-11-17 04:54:25 +0000

    button size

--- resources/views/favorite/favorite_button.blade.php
+++ resources/views/favorite/favorite_button.blade.php
@@ -1,11 +1,11 @@
 <!--@if (Auth::user()->id != $user->id) -->
     @if (Auth::user()->is_favorite($micropost->id))
         {!! Form::open(['route' => ['user.unfavorite', $micropost->id], 'method' => 'delete']) !!}
-            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-block"]) !!}
+            {!! Form::submit('Unfavorite', ['class' => "btn btn-danger btn-xs"]) !!}
         {!! Form::close() !!}
     @else
         {!! Form::open(['route' => ['user.favorite', $micropost->id]]) !!}
-            {!! Form::submit('Favorite',['class' => "btn btn-primary btn-block"]) !!}
+            {!! Form::submit('Favorite',['class' => "btn btn-primary btn-xs"]) !!}
         {!! Form::close() !!}
     @endif
 <!-- @endif -->
